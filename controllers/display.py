491e74cef2bbec88333fc51b53c78a8e0d068481
zhexuan <zhexuanc@umich.edu>
2017-11-06 23:18:44 -0500

    sliding

--- controllers/display.py
+++ controllers/display.py
@@ -36,8 +36,27 @@ def display_route():
 				#print image_id
 		elif request.method == 'GET':
 			pass
-		options['pictures'] = database.get_images(username)
-		options['picid'] = request.args.get('picid')
+		cur_picid = request.args.get('picid')
+		pictures_list = database.get_images(username)
+		cursheet = ''
+		for pictures in pictures_list:
+			if pictures['image_id'] == cur_picid:
+				cursheet = pictures['sheetmusic']
+				break
+		curlist = []
+		for pictures in pictures_list:
+			if pictures['sheetmusic'] == cursheet:
+				curlist.append(pictures)
+		newlist = sorted(curlist, key=lambda k: k['page']) 
+		options['pictures'] = newlist
+		options['picid'] = cur_picid
+		totalpage = len(newlist)
+		for i in range(totalpage):
+			if newlist[i]['image_id'] == cur_picid:
+				if newlist[i]['page'] != 0:
+					options['before'] = newlist[i-1]['image_id']
+				if newlist[i]['page'] != totalpage-1:
+					options['next'] = newlist[i+1]['image_id']
 		print options['picid']
 		return render_template('display.html', **options)
 	except Exception as e:
--- controllers/script.py
+++ controllers/script.py
@@ -6,7 +6,6 @@ import hashlib, datetime
 from base64 import b64encode
 #coding:utf-8
 from wand.image import Image
-import pyPdf
 import os
 
 script = Blueprint('script', __name__, template_folder='templates')
@@ -88,4 +87,3 @@ def script_route():
 		return render_template('script.html', **options)
 	except Exception as e:
 		print str(e)
-
