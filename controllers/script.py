6d167235b563ddd46ea79dbf74994bc8545d59a3
Jiacheng Li <mickeyli@umich.edu>
2017-12-01 07:06:00 +0800

    finish first version

--- controllers/script.py
+++ controllers/script.py
@@ -6,6 +6,50 @@ from base64 import b64encode
 #coding:utf-8
 from wand.image import Image
 import os
+from PIL import Image as PILImage
+import numpy
+from math import trunc
+def splitimage(src, rowrate, dstpath):
+    img = PILImage.open(src)
+    w, h = img.size
+ 
+    print('Original image info: %sx%s, %s, %s' % (w, h, img.format, img.mode))
+    s = os.path.split(src)
+    if dstpath == '':
+        dstpath = s[0]
+    fn = s[1].split('.')
+    basename = fn[0]
+    ext = fn[-1]
+    num = 0
+    #rowheight = h // rownum
+    colwidth = w
+    for i in range(len(rowrate)-1):
+            box = (0, rowrate[i] * h, colwidth, rowrate[i + 1] * h)
+            img.crop(box).save(os.path.join(dstpath, basename + '_' + str(num) + '.' + ext), ext)
+            num = num + 1
+
+def analyzeSheetMusic(src):
+    img = PILImage.open(src)
+    grayImg = img.convert('L')
+    matrix = numpy.asarray(grayImg)
+    h,w = matrix.shape
+    whiteSum = 0
+    ratio=[0]
+    for i in range(h):
+        for j in range(w):
+            if(matrix[i,j]>200):
+                whiteSum+=1
+        if (whiteSum==w):
+            ratioFound = (i+0.0)/h
+            if(ratioFound<0.1):
+                continue
+            if(ratioFound<ratio[len(ratio)-1]+0.1):
+                continue
+            ratio.append(ratioFound)
+        whiteSum=0
+    ratio.append(1)
+    return ratio
+
 
 script = Blueprint('script', __name__, template_folder='templates')
 
@@ -29,22 +73,29 @@ def script_route():
 				if database.check_sheetmusic(username, sheetid)>0:
 					raise RuntimeError('Duplicate sheetmusic uploaded')				
 				image_pdf = Image(filename=filename,resolution=300)
-				image_jpeg = image_pdf.convert('jpg')
+				image_jpeg = image_pdf.convert('png')
 				req_image = []
 				for img in image_jpeg.sequence:
 					img_page = Image(image=img)
-					req_image.append(img_page.make_blob('jpg'))
+					req_image.append(img_page.make_blob('png'))
 				i = 0
 				database.add_sheetmusic(username, sheetid)
 				for img in req_image:
-					ff = open(str(i)+'.jpg','wb')
+					ff = open(str(i)+'.png','wb')
 					ff.write(img)
 					ff.close()
-					image_data = file(str(i)+'.jpg').read()
-					image_id = hashlib.md5(username+str(datetime.datetime.now())).hexdigest()
-					database.add_image(username, image_data, sheetid, i, image_id)
-					os.remove(str(i)+'.jpg')
-					i += 1
+					src = str(i)+'.png'
+					rowlist = analyzeSheetMusic(src)
+				        splitimage(src, rowlist, '')
+					j = 0
+					for imgi in range(len(rowlist)-1):
+						image_data = file(str(i) + '_' + str(imgi) + '.png').read()
+						image_id = hashlib.md5(username+str(datetime.datetime.now())).hexdigest()
+						database.add_image(username, image_data, sheetid, i+j, image_id)
+						j+=1
+						os.remove(str(i) + '_' + str(imgi) + '.png')
+					os.remove(str(i)+'.png')
+					i = i+j
 				os.remove(filename)
 			elif request.form['op'] == 'delete_image':
 				sheetmusic = request.form['sheetmusic']
--- test.py
+++ test.py
@@ -1,21 +1,53 @@
-#coding:utf-8
-from wand.image import Image
+import os
+from PIL import Image
+import numpy
+from math import trunc
+def splitimage(src, rowrate, dstpath):
+    img = Image.open(src)
+    w, h = img.size
  
-# 将pdf文件转为jpg图片文件
-# ./PDF_FILE_NAME 为pdf文件路径和名称
-image_pdf = Image(filename='./t.pdf',resolution=300)
-image_jpeg = image_pdf.convert('jpg')
- 
-# wand已经将PDF中所有的独立页面都转成了独立的二进制图像对象。我们可以遍历这个大对象，并把它们加入到req_image序列中去。
-req_image = []
-for img in image_jpeg.sequence:
-    img_page = Image(image=img)
-    req_image.append(img_page.make_blob('jpg'))
- 
-# 遍历req_image,保存为图片文件
-i = 0
-for img in req_image:
-    ff = open(str(i)+'.jpg','wb')
-    ff.write(img)
-    ff.close()
-    i += 1
+    print('Original image info: %sx%s, %s, %s' % (w, h, img.format, img.mode))
+    s = os.path.split(src)
+    if dstpath == '':
+        dstpath = s[0]
+    fn = s[1].split('.')
+    basename = fn[0]
+    ext = fn[-1]
+    num = 0
+    #rowheight = h // rownum
+    colwidth = w
+    for i in range(len(rowrate)-1):
+            box = (0, rowrate[i] * h, colwidth, rowrate[i + 1] * h)
+            img.crop(box).save(os.path.join(dstpath, basename + '_' + str(num) + '.' + ext), ext)
+            num = num + 1
+
+def analyzeSheetMusic(src):
+    img = Image.open(src)
+    grayImg = img.convert('L')
+    matrix = numpy.asarray(grayImg)
+    h,w = matrix.shape
+    whiteSum = 0
+    ratio=[0]
+    for i in range(h):
+        for j in range(w):
+            if(matrix[i,j]==255):
+                whiteSum+=1
+        if (whiteSum==w):
+            ratioFound = (i+0.0)/h
+            if(ratioFound<0.1):
+                continue
+            if(ratioFound<ratio[len(ratio)-1]+0.1):
+                continue
+            ratio.append(ratioFound)
+        whiteSum=0
+    ratio.append(1)
+    return ratio
+
+src = '0.png'
+if os.path.isfile(src):
+    dstpath = ''
+    if (dstpath == '') or os.path.exists(dstpath):
+	rowlist = analyzeSheetMusic(src)
+        splitimage(src, rowlist, dstpath)
+else:
+    print('notexist')
